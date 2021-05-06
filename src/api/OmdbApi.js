e51e135a6354129f23e5df16f27c5c3ddcb811a4
Arnie <59781783+Barani2396@users.noreply.github.com>
2021-05-05 20:25:18 -0400

    changes in api file

--- src/api/OmdbApi.js
+++ src/api/OmdbApi.js
@@ -2,7 +2,9 @@ import axios from "axios";
 
 export const OmdbApiKey = process.env.REACT_APP_OMDB_API_KEY; //omdb api key
 
+const OmdbUrl = `http://www.omdbapi.com/?i=tt3896198&apikey=${OmdbApiKey}`
+
 //creating axios instance with omdb api
 export default axios.create({
-  baseURL: OmdbApiKey,
+  baseURL: OmdbUrl,
 });
